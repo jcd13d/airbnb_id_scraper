2ae1a8458026faffeff43380345966ab5cc3d419
Justin DiEmmanuele <jdiemmanuele@gmail.com>
2022-07-27 21:34:49 -0400

    new bucket

--- config/constants.py
+++ config/constants.py
@@ -1,6 +1,6 @@
-OCC_CONFIG_LOCATION = "s3://airbnb-scraper-bucket-0-0-1/running_configs/config_occ.json"
-PRICING_CONFIG_LOCATION = "s3://airbnb-scraper-bucket-0-0-1/running_configs/config_price.json"
-REVIEW_CONFIG_LOCATION = "s3://airbnb-scraper-bucket-0-0-1/running_configs/config_reviews.json"
-ID_CONFIG_LOCATION = "s3://airbnb-scraper-bucket-0-0-1/running_configs/id_config.json"
+OCC_CONFIG_LOCATION = "s3://airbnb-scraper-bucket-0-1-1/running_configs/config_occ.json"
+PRICING_CONFIG_LOCATION = "s3://airbnb-scraper-bucket-0-1-1/running_configs/config_price.json"
+REVIEW_CONFIG_LOCATION = "s3://airbnb-scraper-bucket-0-1-1/running_configs/config_reviews.json"
+ID_CONFIG_LOCATION = "s3://airbnb-scraper-bucket-0-1-1/running_configs/id_config.json"
 
 NUM_REQUEST_TRIES = 10
